e5e84d41fa10cb59b063964e655247d742ec87ae
Peter Kolosov <101456132+Peter-developer01@users.noreply.github.com>
2023-10-28 09:47:29 +0300

    final result of theory practice

--- module-1/5-6_jsx/src/index.js
+++ module-1/5-6_jsx/src/index.js
@@ -7,15 +7,27 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const Header = () => {
+  // const scr = "<script>alert('Error!')</script>"
+  // return <h2>{scr}</h2>
   return <h2>Hello World!</h2>
 }
 
 const Field = () => {
-  return <input type="text" placeholder="Type here"/>
+  const holder = "Enter here"
+  return <input type="text" placeholder={holder}/>
 }
 
 const Button = () => {
-  return <button/>
+  // const res = () => {
+  //   return "Log in, please"
+  // }
+
+  // const p = <p>{res()}</p>
+  // return <button>{p}</button>
+  // return <button>{4+4}</button>
+  const text = "Log in"
+  const logged = true
+  return <button className="first">{logged ? "Enter" : text}</button>
 }
 
 const App = () => {
