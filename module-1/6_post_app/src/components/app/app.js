9a3b8bb5add61deeb3ef436870d6d79737f8bc7e
Peter Kolosov <101456132+Peter-developer01@users.noreply.github.com>
2023-10-28 11:05:41 +0300

    create component PostStatusFilter

--- module-1/6_post_app/src/components/app/app.js
+++ module-1/6_post_app/src/components/app/app.js
@@ -1,6 +1,8 @@
 import React from 'react';
+
 import AppHeader from '../app-header'
 import SearchPanel from '../search-panel'
+import PostStatusFilter from '../post-status-filter';
 
 const App = () => {
     return (
@@ -8,6 +10,7 @@ const App = () => {
             <AppHeader/>
             <div className="search-panel d-flex">
                 <SearchPanel/>
+                <PostStatusFilter/>
             </div>
         </div>
     )
