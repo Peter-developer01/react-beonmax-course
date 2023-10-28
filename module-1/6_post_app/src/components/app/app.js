6755d12f0787429cca4c88fb85653123973da9ee
Peter Kolosov <101456132+Peter-developer01@users.noreply.github.com>
2023-10-28 10:59:19 +0300

    create component SearchPanel

--- module-1/6_post_app/src/components/app/app.js
+++ module-1/6_post_app/src/components/app/app.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import AppHeader from '../app-header'
+import SearchPanel from '../search-panel'
 
 const App = () => {
     return (
-        <AppHeader/>
+        <div className="app">
+            <AppHeader/>
+            <div className="search-panel d-flex">
+                <SearchPanel/>
+            </div>
+        </div>
     )
 }
 
