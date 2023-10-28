7d4ec19e083d91602eeed37a5b267c6f75c89b2d
Peter Kolosov <101456132+Peter-developer01@users.noreply.github.com>
2023-10-28 09:08:08 +0300

    edited index.js to be more simple for starting

--- module-1/5-6_jsx/src/index.js
+++ module-1/5-6_jsx/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+const elem = (
+  <div>
+    <h2>Hello World!</h2>
+    <input type="text" placeholder="Type here"/>
+    <button/>
+  </div>
 );
+
+root.render(elem);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
