16460f19dd898a80d22ff7168f8ac6eeba56601c
Peter Kolosov <101456132+Peter-developer01@users.noreply.github.com>
2023-10-29 17:28:19 +0300

    add the logic for making post important

--- module-1/6_post_app/src/components/post-list-item/post-list-item.js
+++ module-1/6_post_app/src/components/post-list-item/post-list-item.js
@@ -1,29 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import '../../css/post-list-item.css'
 
-const PostListItem = ({label, important = false}) => {
-    let classNames = 'app-list-item d-flex justify-content-between';
+export default class PostListItem extends Component {
+    constructor(props) {
+        super(props)
+        this.onImportant = this.onImportant.bind(this)
+        this.state = {
+            important: props.important
+        }
+    }
 
-    return (
-        <div className={classNames + (important ? ' important' : '')}>
-            <span className="app-list-item-label">
-                {label}
-            </span>
+    onImportant() {
+        this.setState(({ important }) => ({
+            important: !important
+        }))
+    }
 
-            <div className="d-flex justify-content-center align-items-center">
-                <button className="btn-star btn-sm">
-                    <i className="fa fa-star"></i>
-                </button>
+    render() {
+        const { label } = this.props
+        const { important } = this.state
+        let classNames = 'app-list-item d-flex justify-content-between';
 
-                <button className="btn-trash btn-sm">
-                    <i className="fa fa-trash-o"></i>
-                </button>
+        return (
+            <div className={classNames + (important ? ' important' : '')}>
+                <span className="app-list-item-label">
+                    {label}
+                </span>
 
-                <i className="fa fa-heart"></i>
+                <div className="d-flex justify-content-center align-items-center">
+                    <button
+                        type="button"
+                        className="btn-star btn-sm"
+                        onClick={this.onImportant}>
+                        <i className="fa fa-star"></i>
+                    </button>
+
+                    <button
+                        type="button"
+                        className="btn-trash btn-sm">
+                        <i className="fa fa-trash-o"></i>
+                    </button>
+
+                    <i className="fa fa-heart"></i>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
-
-export default PostListItem
