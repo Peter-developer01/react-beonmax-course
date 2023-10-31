c3ec84d68b052c3dc47b9961782a69402a9b63cf
Peter Kolosov <101456132+Peter-developer01@users.noreply.github.com>
2023-10-31 10:42:33 +0300

    add some buttons

--- module-1/10_styled_components/src/App.js
+++ module-1/10_styled_components/src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <ButtonGroup>
+            <Button variant="outline-info" onClick={() => alert('Primary clicked!')}>Primary</Button>
+            <Button variant="outline-danger" onClick={() => alert('Danger clicked!')}>Be careful</Button>
+            <Button variant="outline-success" onClick={() => alert('Success clicked!')}>Success</Button>
+            <Button variant="outline-warning" onClick={() => alert('Warning clicked!')}>Warning!</Button>
+          </ButtonGroup>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
